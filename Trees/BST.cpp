e934a4881445a31282424c95733dc02bafaf63a8
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2021-05-06 23:04:33 +0530

    Create BST.cpp

---
+++ Trees/BST.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+#include <cstddef>
+#include <stdlib.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int data){
+            this->data=data;
+            left=right=NULL;
+        }
+};
+
+class BST{
+    public:
+        Node * insert(Node *root,int x){
+            Node *n = new Node(x);
+            if(root==NULL){
+                root=n;
+            }
+            else if(root->data>x){
+                root->left=insert(root->left,x);
+            }
+            else{
+                root->right=insert(root->right,x);
+            }
+            return root;
+        }
+        void inorder(Node *root){
+            if(root!=NULL){
+                inorder(root->left);
+                cout<<root->data<<" ";
+                inorder(root->right);
+            }
+        }
+        void preorder(Node *root){
+            if(root!=NULL){
+                cout<<root->data<<" ";
+                preorder(root->left);
+                preorder(root->right);
+            }
+        }
+        void postorder(Node *root){
+            if(root!=NULL){
+                postorder(root->left);
+                postorder(root->right);
+                cout<<root->data<<" ";
+            }
+        }
+        int height(Node *root){
+            if(root==NULL){
+                return 0;
+            }
+            else{
+                return max(height(root->left),height(root->right))+1;
+            }
+        }
+        bool search(Node *root,int x){
+            if(root==NULL){
+                return false;
+            }
+            else if(root->data==x){
+                return true;
+            }
+            else if(root->data>x){
+                return search(root->left,x);
+            }
+            else{
+                return search(root->right,x);
+            }
+        }
+        int get_max(Node *root){
+            if(root==NULL){
+                return -1;
+            }
+            else{
+                if(root->left==NULL){
+                    return root->data;
+                }
+                else{
+                    return get_max(root->left);
+                }
+            }
+        }
+        int get_min(Node *root){
+            if(root==NULL){
+                return -1;
+            }
+            else{
+                if(root->right==NULL){
+                    return root->data;
+                }
+                else{
+                    return get_min(root->right);
+                }
+            }
+        }
+};
+
+int main(){
+    BST t;
+    Node *root;
+    cout<<"hello";
+    root = t.insert(root, 15);
+    root = t.insert(root, 10);
+    root = t.insert(root, 20);
+    root = t.insert(root, 25);
+    root = t.insert(root, 8);
+    root = t.insert(root, 12);
+    int number;
+    cin >> number;
+    if (t.search(root, number)) cout << "Found\n";
+    else cout << "Not Found\n";
+
+    cout << t.get_min(root) << endl;
+    cout << t.get_max(root) << endl;
+
+    //cout << "Level order: ";
+    //LevelOrder(root);
+    //cout << endl;
+
+    cout << "PreOrder: ";
+    t.preorder(root);
+    cout << endl;
+
+    cout << "InOrder: ";
+    t.inorder(root);
+    cout << endl;
+
+    cout << "PostOrder: ";
+    t.postorder(root);
+    cout << endl;
+    return 0;
+}
