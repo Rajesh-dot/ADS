a267fe1635b33943a4337276ae32f9127ebac3c7
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2021-05-06 22:59:23 +0530

    Create DigitFolding.cpp

---
+++ Hashing/DigitFolding.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+class HashTable{
+	private:
+		int *h;
+		int size;
+	public:
+		HashTable(int size)
+	    {
+	        this->size = size;
+	        h = (int *)calloc(size, sizeof(int));
+	    }
+	    // Digit Folding
+
+	    int find_len(int x){
+	        int c=0;
+	        if(x==0){
+                c=1;
+	        }
+	        while(x!=0){
+                x=x/10;
+                c+=1;
+	        }
+	        return c;
+	    }
+
+
+	    int find_sum(int x){
+	        int t=find_len(x);
+	        int left=0,right=0;
+            t=t/2;
+            int c=0;
+            int mul=1;
+            while(x!=0){
+                if(c<t){
+                    left=left+(x%10)*mul;
+                }
+                else if(c==t){
+                    mul=1;
+                    right=right+(x%10)*mul;
+                }
+                else{
+                    right=right+(x%10)*mul;
+                }
+                x=x/10;
+                c+=1;
+                mul=mul*10;
+            }
+            cout<<left<<" "<<right<<endl;
+	        return left+right;
+	    }
+
+	    int f(int x){
+	        int t=find_sum(x);
+			return t%size;
+		}
+
+		void add(int x){
+			h[f(x)]=x;
+		}
+
+		bool search(int x){
+			if(h[f(x)]==x){
+				return true;
+			}
+			return false;
+		}
+
+
+		void del(int x){
+			if(search(x)){
+				h[f(x)]=0;
+			}
+		}
+
+		void display(){
+			for(int i=0;i<size;i++){
+				cout<<"[ "<<i<<" : "<<h[i]<<" ]";
+			}
+			cout<<endl;
+		}
+};
+
+
+int main(){
+	int size;
+	cout<<"Enter size: "<<endl;
+	cin>>size;
+	int input,choice,id;
+	HashTable h(size);
+	while(true){
+		cout<<"Enter choice"<<endl;
+		cout<<"1.) insert\n2.) delete\n3.) search\n4.) quit\n";
+		cin>> choice;
+		switch(choice){
+			case 1:
+				cout<<"Enter input: ";
+				cin>>input;
+				h.add(input);
+				h.display();
+				break;
+			case 2:
+				cout<<"Enter input: ";
+				cin>>input;
+				h.del(input);
+				h.display();
+				break;
+			case 3:
+				cout<<"Enter input: ";
+				cin>>input;
+				if(h.search(input)){
+					cout<<"Found"<<endl;
+				}
+				else{
+					cout<<"Not Found"<<endl;
+				}
+				break;
+			case 4:
+				exit(0);
+				break;
+			default:
+				cout<<"Wrong choce"<<endl;
+				break;
+		}
+	}
+	return 0;
+}
