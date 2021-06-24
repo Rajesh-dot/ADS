4def9246a903c1503510485a64b66e2821b18dd1
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2021-06-24 10:25:02 +0530

    Update BST_ITERATIVE.cpp

--- Trees/BST_ITERATIVE.cpp
+++ Trees/BST_ITERATIVE.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <stack>
+#include <queue>
 #include <stdlib.h>
 using namespace std;
 
@@ -71,6 +72,89 @@ class BST{
                 }
             }
         }
+        bool search(Node *root,int x){
+            if(root==NULL){
+                return false;
+            }
+            Node* curr = root;
+            while(curr!=NULL){
+                if(curr->data==x){
+                    return true;
+                }
+                if(curr->data<x){
+                    curr=curr->right;
+                }
+                else{
+                    curr=curr->left;
+                }
+            }
+            return false;
+        }
+        Node* del(Node* root,int x){
+            if(root==NULL){
+                return root;
+            }
+            Node* curr = root;
+            Node* prev = NULL;
+            while(curr!=NULL){
+                if(curr->data==x){
+                    if(curr->left==NULL || curr->right==NULL){
+                        if(curr->left==NULL){
+                            if(prev->left->data==curr->data){
+                                prev->left=curr->right;
+                            }
+                            else{
+                                prev->right=curr->right;
+                            }
+                        }
+                        else{
+                            if(prev->left->data==curr->data){
+                                prev->left=curr->left;
+                            }
+                            else{
+                                prev->right=curr->left;
+                            }
+                        }
+                        free(curr);
+                    }
+                    else{
+                        Node* p = NULL;
+                        Node* temp;
+                        temp = curr->right;
+                        while (temp->left != NULL) {
+                            p = temp;
+                            temp = temp->left;
+                        }
+                        if (p != NULL){
+                            p->left = temp->right;
+                        }
+                        else{
+                            curr->right = temp->right;
+                        }
+
+                        curr->data = temp->data;
+                        free(temp);
+                    }
+                    return root;
+                }
+                prev = curr;
+                if(curr->data<x){
+                    curr=curr->right;
+                }
+                else{
+                    curr=curr->left;
+                }
+            }
+            cout<<"Element not found"<<endl;
+            return root;
+        }
+        Node* get_min(Node *root){
+            Node* curr=root;
+            while(curr->left!=NULL){
+                curr=curr->left;
+            }
+            return curr;
+        }
 };
 int main(){
     BST t;
@@ -82,6 +166,13 @@ int main(){
     t.insert(root, 8);
     t.insert(root, 12);
 
+    cout << "Inorder: ";
+    t.inorder(root);
+    cout << endl;
+    cout << "Search: ";
+    cout<<t.search(root,5);
+    cout << endl;
+    root=t.del(root,20);
     cout << "Inorder: ";
     t.inorder(root);
     cout << endl;
