a5c4dc39c768ea6f8f10a94cd9353e0b4d234659
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2021-08-14 10:34:20 +0530

    Added deletion

--- Trees/AVL_TREE.cpp
+++ Trees/AVL_TREE.cpp
@@ -107,25 +107,95 @@ class AVL{
             }
             if(x<root->data){
                 root->left = del(root->left,x);
+                root->height = 1+max(height(root->left),height(root->right));
+                int bf = balanceFactor(root);
+                if(bf>1 && x<root->left->data){
+                    return right_rotate(root);
+                }
+                if(bf<-1 && x>root->right->data){
+                    return left_rotate(root);
+                }
+                if(bf>1 && x>root->left->data){
+                    root->left = left_rotate(root->left);
+                    return right_rotate(root);
+                }
+                if(bf<-1 && x<root->right->data){
+                    root->right = right_rotate(root->right);
+                    return left_rotate(root);
+                }
             }
             else if(x>root->data){
                 root->right = del(root->right,x);
+                root->height = 1+max(height(root->left),height(root->right));
+                int bf = balanceFactor(root);
+                if(bf>1 && x<root->left->data){
+                    return right_rotate(root);
+                }
+                if(bf<-1 && x>root->right->data){
+                    return left_rotate(root);
+                }
+                if(bf>1 && x>root->left->data){
+                    root->left = left_rotate(root->left);
+                    return right_rotate(root);
+                }
+                if(bf<-1 && x<root->right->data){
+                    root->right = right_rotate(root->right);
+                    return left_rotate(root);
+                }
             }
             else{
-
+                if(root->right==NULL && root->left==NULL){
+                    root=NULL;
+                    return root;
+                }
+                if(root->left!=NULL && root->right!=NULL){
+                    int temp = find_min(root->right);
+                    root->data = temp;
+                    root->right = del(root->right,temp);
+                }
+                else if(root->left==NULL){
+                    root->data=root->right->data;
+                    root->right=del(root->right,root->right->data);
+                }
+                else{
+                    root->data=root->left->data;
+                    root->left=del(root->left,root->left->data);
+                }
+                return root;
             }
+            return root;
+        }
+        int find_min(Node *root){
+            if(root==NULL){
+                return 0;
+            }
+            while(root->left!=NULL){
+                root=root->left;
+            }
+            return root->data;
         }
 };
 
 int main(){
     AVL t;
-    Node *root=NULL;
-    root = t.insert(root, 15);
     root = t.insert(root, 10);
     root = t.insert(root, 20);
-    root = t.insert(root, 25);
+    root = t.insert(root, 30);
     root = t.insert(root, 8);
-    root = t.insert(root, 12);
+    root = t.insert(root, 4);
+    root = t.insert(root, 40);
+    root = t.insert(root, 35);
+    root = t.insert(root, 17);
+    root = t.insert(root, -1);
+
+    cout << "InOrder: ";
+    t.inorder(root);
+    cout << endl;
+
+    root = t.del(root,40);
+    root = t.del(root,20);
+    root = t.del(root,-1);
+    root = t.del(root,4);
 
     cout << "InOrder: ";
     t.inorder(root);
