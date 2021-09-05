e40fdd7d1941ddb7db774a4e1bd7671e2a29afd1
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2021-09-05 11:05:23 +0530

    Update Krushkal.cpp

--- Graphs/Krushkal.cpp
+++ Graphs/Krushkal.cpp
@@ -16,18 +16,6 @@ bool compareData(Data i1, Data i2)
 }
 
 
-void print(map<int , vector< pair<int, int > > > adjList){
-    for(map<int , vector< pair<int, int > > >::iterator it = adjList.begin() ; it != adjList.end() ; it++){
-        cout << it->first << " : ";
-        vector<pair<int, int> > neighbours = it->second;
-        for(int i = 0 ; i < neighbours.size() ; i++){
-          cout << "(" << neighbours[i].first << " " << neighbours[i].second << ")";
-        }
-        cout << endl;
-    }
-}
-
-
 int find(int parents[], int vertex){
     if(parents[vertex]!=vertex)
         return find(parents, parents[vertex]);;
@@ -44,36 +32,23 @@ void make_union(int parents[], int x, int y){
 
 int main()
 {
-    map<int,vector<pair<int,int> > > graph;
-    vector<int> vertices;
     int n,vertex,starting,ending,weight,m;
 
 
     cout<<"Enter no of vertices: "<<endl;
     cin>>n;
 
-
-    for(int i=0;i<n;i++){
-        vertices.push_back(i+1);
-    }
-
     vector<Data> edges;
-
 
     cout<<"Enter no of edges - (starting,ending,weight): "<<endl;
     cin>>m;
+
     cout<<"Enter edges: "<<endl;
 
     for(int i=0;i<m;i++){
         cin>>starting>>ending>>weight;
         edges.push_back({starting,ending,weight});
-        graph[starting].push_back(make_pair(ending,weight));
-        graph[ending].push_back(make_pair(starting,weight));
     }
-
-
-    cout<<"Graph:"<<endl;
-    print(graph);
 
 
     int parents[n+1];
@@ -103,12 +78,12 @@ int main()
 Sample input:-
 5
 7
-1 2 5
-2 5 2
-2 4 3
-1 4 7
-3 4 1
+0 1 5
+1 4 2
 1 3 3
-4 5 2
+0 3 7
+2 3 1
+0 2 3
+3 4 2
 
 */
